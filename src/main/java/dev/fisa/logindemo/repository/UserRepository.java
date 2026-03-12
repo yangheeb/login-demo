211829b0607dfbd293d0a553d0246e1f310e2a04
seonjiwon <tommys915@gmail.com>
2026-03-12 11:36:57 +0900

    feat: init

--- src/main/java/dev/fisa/logindemo/repository/UserRepository.java
+++ src/main/java/dev/fisa/logindemo/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package dev.fisa.logindemo.repository;
 
+import dev.fisa.logindemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
